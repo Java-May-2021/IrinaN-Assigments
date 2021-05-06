4a3003953128e2e2db7a960172cb8f77f59e1902
ivnizhnik <77816282+ivnizhnik@users.noreply.github.com>
2021-05-05 20:21:59 -0500

    Add files via upload

---
+++ 01-java-fundamentals/MyName.java
@@ -0,0 +1,8 @@
+
+public class MyName {
+    public static void main(String[] args) {
+        System.out.println("My name is Ninja Irina");
+        System.out.println("I am 41 years old");
+        System.out.println("I currently live in Chicago, IL");
+    }
+}
