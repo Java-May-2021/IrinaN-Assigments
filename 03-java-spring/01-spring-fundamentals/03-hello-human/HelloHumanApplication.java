e364017599574454ae3199cf36f58e0531033dbb
ivnizhnik <77816282+ivnizhnik@users.noreply.github.com>
2021-05-23 18:41:41 -0500

    Add files via upload

---
+++ 03-java-spring/01-spring-fundamentals/03-hello-human/HelloHumanApplication.java
@@ -0,0 +1,13 @@
+package com.irina.helloHuman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloHumanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloHumanApplication.class, args);
+	}
+
+}
