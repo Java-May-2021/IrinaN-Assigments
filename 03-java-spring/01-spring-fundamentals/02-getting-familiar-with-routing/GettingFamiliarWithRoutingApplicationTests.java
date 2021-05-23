f584dc4697ac739c59487d95011c858d7fe26bc9
ivnizhnik <77816282+ivnizhnik@users.noreply.github.com>
2021-05-23 18:38:48 -0500

    Add files via upload

---
+++ 03-java-spring/01-spring-fundamentals/02-getting-familiar-with-routing/GettingFamiliarWithRoutingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irina.gettingFamiliarWithRouting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GettingFamiliarWithRoutingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
